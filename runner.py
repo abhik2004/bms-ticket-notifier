3721134d423382a59b2c011ec4abdc22289c5e64
Abhik Kumar <116881195+abhik2004@users.noreply.github.com>
2026-03-31 17:00:30 +0530

    Update subprocess command and add comment for interval

--- runner.py
+++ runner.py
@@ -1,13 +1,13 @@
 import time
 import subprocess
 
-INTERVAL = 300
+INTERVAL = 300  # change to 60 if you want 1 min
 
 while True:
     start = time.time()
 
     print("Running BMS checker...")
-    subprocess.run(["python", "main.py"])
+    subprocess.run(["uv", "run", "main.py"])
 
     elapsed = time.time() - start
     sleep_time = max(0, INTERVAL - elapsed)
