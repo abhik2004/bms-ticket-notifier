80679d6396ddc781e6f89e53cca7cf4f63130125
Abhik Kumar <116881195+abhik2004@users.noreply.github.com>
2026-04-02 17:40:20 +0530

    Change sleep interval to 360 seconds and adjust jitter
    Reduced interval and jitter range for sleep time.

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import time
 import random
 import subprocess
 
-INTERVAL = 480  # change to 60 if you want 1 min
+INTERVAL = 360  # change to 60 if you want 1 min
 
 while True:
     start = time.time()
@@ -12,8 +12,8 @@ while True:
 
     elapsed = time.time() - start
 
-    jitter = random.randint(-120, 120)  # +/- 1 min
-    sleep_time = max(120, INTERVAL + jitter - elapsed)
+    jitter = random.randint(-60, 60)  # +/- 1 min
+    sleep_time = max(60, INTERVAL + jitter - elapsed)
 
     print(f"Sleeping for {sleep_time} seconds...\n")
     time.sleep(sleep_time)
