3ba162436ad8ce0c3afe3a807f2e3b23ed0d748e
Abhik Kumar <116881195+abhik2004@users.noreply.github.com>
2026-04-02 16:02:16 +0530

    Modify interval and implement sleep jitter
    Adjust the interval to 480 seconds and add jitter to sleep time.

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 import time
 import subprocess
 
-INTERVAL = 600  # change to 60 if you want 1 min
+INTERVAL = 480  # change to 60 if you want 1 min
 
 while True:
     start = time.time()
@@ -10,7 +10,9 @@ while True:
     subprocess.run(["uv", "run", "main.py"])
 
     elapsed = time.time() - start
-    sleep_time = max(0, INTERVAL - elapsed)
+
+    jitter = random.randint(-120, 120)  # +/- 1 min
+    sleep_time = max(120, INTERVAL + jitter - elapsed)
 
     print(f"Sleeping for {sleep_time} seconds...\n")
     time.sleep(sleep_time)
